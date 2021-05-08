7b1d463a429ed25153c66d86674fd14d596fb153
Chris Brown <cbrown1234@hotmail.co.uk>
2021-05-08 19:02:48 +0100

    Optimise key names collection

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use serde_json::Value;
+use itertools::sorted;
 
 fn main() {
     let contents = std::fs::read_to_string("test_data/some_data.json").unwrap();
 
-    let mut keys = std::collections::HashSet::new();
     let mut keys_count = std::collections::HashMap::new();
     let mut line_count: i64 = 0;
 
@@ -11,13 +11,13 @@ fn main() {
         let v: Value = serde_json::from_str(line).unwrap();
 
         for (key, _) in v.as_object().unwrap() {
-            keys.insert(key.to_string());
-            let counter = keys_count.entry(key.to_string()).or_insert(0);
+            let counter = keys_count.entry(key.to_owned()).or_insert(0);
             *counter += 1;
         }
         line_count += 1;
     }
 
+    let keys = sorted(keys_count.keys()); 
     println!("Keys:\n{:#?}", keys);
     println!("Key value counts:\n{:#?}", keys_count);
     for (k, v) in keys_count {
