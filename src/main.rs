93e40c7c49e16ae512f49047df00bb5524521d5d
Chris Brown <cbrown1234@hotmail.co.uk>
2021-05-14 00:26:16 +0100

    Add basic CLI args

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use itertools::sorted;
 use serde_json::Value;
+use structopt::StructOpt;
 
 fn main() {
-    let contents = std::fs::read_to_string("test_data/some_data.json").unwrap();
+    let args = Cli::from_args();
+
+    let content = std::fs::read_to_string(&args.file_path).expect("could not read file");
 
     let mut keys_count = std::collections::HashMap::new();
     let mut line_count: i64 = 0;
 
-    for line in contents.lines() {
+    for line in content.lines() {
         let v: Value = serde_json::from_str(line).unwrap();
 
         for (key, _) in v.as_object().unwrap() {
@@ -23,4 +26,10 @@ fn main() {
     for (k, v) in keys_count {
         println!("{}: {}%", k, 100f64 * v as f64 / line_count as f64)
     }
+}
+
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(parse(from_os_str))]
+    file_path: std::path::PathBuf,
 }
