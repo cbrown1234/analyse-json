281ad9224fec5f68a11c574a469de988c6158b51
Chris Brown <cbrown1234@hotmail.co.uk>
2021-05-18 22:16:45 +0100

    Add JSON path parser

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,8 @@ fn main() {
             *counter += 1;
         }
         line_count += 1;
+
+        println!("{:#?}", parse_json_paths(&v));
     }
 
     let keys = sorted(keys_count.keys());
@@ -26,6 +28,36 @@ fn main() {
     for (k, v) in keys_count {
         println!("{}: {}%", k, 100f64 * v as f64 / line_count as f64)
     }
+}
+
+fn parse_json_paths(json: &Value) -> Vec<String> {
+    let root = String::from("$");
+    let mut paths = Vec::new();
+    _parse_json_paths(json, root, &mut paths);
+    paths
+}
+
+fn _parse_json_paths<'a>(
+    json: &Value,
+    root: String,
+    paths: &'a mut Vec<String>,
+) -> &'a mut Vec<String> {
+    match json {
+        Value::Object(map) => {
+            for (k, v) in map {
+                let mut obj_root = root.clone();
+                obj_root.push_str(".");
+                obj_root.push_str(k);
+                _parse_json_paths(v, obj_root, paths);
+            }
+        }
+        Value::Null => paths.push(root),
+        Value::Bool(_) => paths.push(root),
+        Value::Number(_) => paths.push(root),
+        Value::String(_) => paths.push(root),
+        Value::Array(_) => paths.push(root),
+    }
+    paths
 }
 
 #[derive(StructOpt)]
