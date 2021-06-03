3ee8a5333584d5621c449cdd922481d2737270a8
Chris Brown <cbrown1234@hotmail.co.uk>
2021-06-03 22:38:04 +0100

    Refactor main

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use std::error::Error;
+use std::fs::File;
+use structopt::StructOpt;
+
+mod json;
+
+#[derive(StructOpt)]
+pub struct Cli {
+    #[structopt(parse(from_os_str))]
+    file_path: std::path::PathBuf,
+}
+
+pub fn run(args: Cli)  -> Result<(), Box<dyn Error>> {
+    let file = File::open(&args.file_path)?;
+
+    let file_stats = json::ndjson::parse_ndjson_file(file);
+
+    println!("{}", file_stats);
+
+    Ok(())
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
-use std::fs::File;
+use std::process;
+use analyse_json::Cli;
 use structopt::StructOpt;
-
-mod json;
 
 fn main() {
     let args = Cli::from_args();
 
-    let file = File::open(&args.file_path).expect("could not read file");
+    if let Err(e) = analyse_json::run(args) {
+        println!("Application error: {}", e);
 
-    let file_stats = json::ndjson::parse_ndjson_file(file);
-
-    println!("{}", file_stats);
+        process::exit(1);
+    }
 }
 
-#[derive(StructOpt)]
-struct Cli {
-    #[structopt(parse(from_os_str))]
-    file_path: std::path::PathBuf,
-}
+
