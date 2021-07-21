3f60937a007afc8b77e0dd855a3efe612f0736dd
Chris Brown <cbrown1234@hotmail.co.uk>
2021-07-21 22:54:13 +0100

    Add glob option

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use glob::glob;
 use std::error::Error;
 use std::fs::File;
 use structopt::StructOpt;
@@ -7,15 +8,32 @@ pub mod json;
 #[derive(StructOpt)]
 pub struct Cli {
     #[structopt(parse(from_os_str))]
-    file_path: std::path::PathBuf,
+    file_path: Option<std::path::PathBuf>,
+    
+    #[structopt(short, long)]
+    glob: Option<String>
 }
 
 pub fn run(args: Cli) -> Result<(), Box<dyn Error>> {
-    let file = File::open(&args.file_path)?;
-
-    let file_stats = json::ndjson::parse_ndjson_file(file);
-
-    println!("{}", file_stats);
+    if let Some(file_path) = args.file_path {
+        let file = File::open(file_path)?;
+        let file_stats = json::ndjson::parse_ndjson_file(file);
+        println!("{}", file_stats);
+        return Ok(());
+    }
+    if let Some(pattern) = args.glob {
+        for entry in glob(&pattern)? {
+            match entry {
+                Ok(path) => {
+                    println!("File '{}':", path.display());
+                    let file = File::open(path)?;
+                    let file_stats = json::ndjson::parse_ndjson_file(file);
+                    println!("{}", file_stats);
+                },
+                Err(e) => eprintln!("Error reading glob entry: {:?}", e),
+            }
+        }
+    }
 
     Ok(())
 }
