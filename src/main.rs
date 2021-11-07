e0bb4bfc930d98162862a8bb78d79fd6378f1e67
Chris Brown <cbrown1234@hotmail.co.uk>
2021-11-07 21:14:35 +0000

    Add timer to CLI output

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use analyse_json::Cli;
-use std::process;
+use humantime::format_duration;
+use std::{process, time::Instant};
 use structopt::StructOpt;
 
 fn main() {
     let args = Cli::from_args();
 
+    let now = Instant::now();
     if let Err(e) = analyse_json::run(args) {
         eprintln!("Application error: {}", e);
 
         process::exit(1);
     }
+    println!("Completed in {}", format_duration(now.elapsed()));
 }
