ee796a8516438a14140be7ab0229279c0934c043
Chris Brown <cbrown1234@hotmail.co.uk>
2022-12-03 18:43:08 +0000

    Causes compiler error

--- src/json/ndjson.rs
+++ src/json/ndjson.rs
@@ -48,6 +48,15 @@ pub fn parse_bufreader<'a>(
     Ok(Box::new(json_iter))
 }
 
+pub fn parse_ndjson_file<'a>(
+    args: &Cli,
+    file: File,
+    errors: &Rc<RefCell<Vec<IdErr>>>,
+) -> Result<IdJSONIter<'a>, Box<dyn Error>> {
+    let reader = io::BufReader::new(file);
+    parse_bufreader(args, reader, errors)
+}
+
 pub fn parse_ndjson_file_path<'a>(
     args: &Cli,
     file_path: &PathBuf,
@@ -299,11 +308,11 @@ pub fn expand_jsonpath_query<'a>(
 ) -> IdJSONIter<'a> {
     let select_errors = Rc::clone(errors);
     let missing = Rc::clone(errors);
-    let expanded: IdJSONIter<'a>;
+    let json_iter_out: IdJSONIter<'a>;
     if let Some(ref selector) = settings.jsonpath_selector {
         let path = settings.args.jsonpath.to_owned();
         let path = path.expect("must exits for jsonpath_selector to exist");
-        let expanded_unindexed = json_iter.flat_map(move |(ref id, ref json)| {
+        let expanded = json_iter.flat_map(move |(ref id, ref json)| {
             let mut select_errored = false;
             let selected = selector.select(json).unwrap_or_else(|e| {
                 select_errors
@@ -320,17 +329,15 @@ pub fn expand_jsonpath_query<'a>(
             selected
                 .into_iter()
                 .map(|json| (format!("{id}:{path}"), json.to_owned()))
+                .enumerate()
+                .map(|(i, (id, json))| (format!("{id}[{i}]"), json))
                 .collect::<Vec<_>>()
         });
-        expanded = Box::new(
-            expanded_unindexed
-                .enumerate()
-                .map(|(i, (id, json))| (format!("{id}[{i}]"), json)),
-        );
+        json_iter_out = Box::new(expanded);
     } else {
-        expanded = Box::new(json_iter);
+        json_iter_out = Box::new(json_iter);
     }
-    expanded
+    json_iter_out
 }
 
 pub fn apply_settings<'a>(
@@ -372,6 +379,7 @@ pub fn process_json_iterable(
     fs
 }
 
+#[deprecated]
 pub fn parse_json_iterable<E: 'static + Error>(
     args: &Cli,
     json_iter: impl Iterator<Item = Result<String, E>>,
@@ -421,6 +429,7 @@ pub fn parse_json_iterable<E: 'static + Error>(
     Ok(fs)
 }
 
+#[deprecated]
 pub fn parse_json_iterable_par<E>(
     args: &Cli,
     json_iter: impl Iterator<Item = Result<String, E>> + Send,
@@ -541,6 +550,7 @@ where
     }
 }
 
+#[deprecated]
 pub fn parse_ndjson_bufreader(
     args: &Cli,
     bufreader: impl BufRead + Send,
@@ -549,24 +559,98 @@ pub fn parse_ndjson_bufreader(
     parse_json_iterable_par(args, json_iter)
 }
 
-pub fn parse_ndjson_file(args: &Cli, file: File) -> Result<FileStats, Box<dyn error::Error>> {
-    // if file.metadata().
-    parse_ndjson_bufreader(args, io::BufReader::new(file))
-}
-
 #[cfg(test)]
 mod tests {
+    use serde_json::json;
+
     use super::*;
     use std::fs::File;
     use std::io::{Seek, SeekFrom, Write};
 
     #[test]
-    fn simple_ndjson_file() {
+    fn simple_ndjson() {
         let mut tmpfile: File = tempfile::tempfile().unwrap();
         writeln!(tmpfile, r#"{{"key1": 123}}"#).unwrap();
         writeln!(tmpfile, r#"{{"key2": 123}}"#).unwrap();
         writeln!(tmpfile, r#"{{"key1": 123}}"#).unwrap();
         tmpfile.seek(SeekFrom::Start(0)).unwrap();
+        let reader = io::BufReader::new(tmpfile);
+
+        let expected: Vec<IdJSON> = vec![
+            (0.to_string(), json!({"key1": 123})),
+            (1.to_string(), json!({"key2": 123})),
+            (2.to_string(), json!({"key1": 123})),
+        ];
+
+        let args = Cli::default();
+        let errors = Rc::new(RefCell::new(vec![]));
+
+        let json_iter = parse_bufreader(&args, reader, &errors).unwrap();
+        assert_eq!(expected, json_iter.collect::<Vec<IdJSON>>());
+        assert!(errors.borrow().is_empty())
+    }
+
+    #[test]
+    fn bad_ndjson_file() {
+        let mut tmpfile: File = tempfile::tempfile().unwrap();
+        writeln!(tmpfile, r#"{{"key1": 123}}"#).unwrap();
+        writeln!(tmpfile, r#"not valid json"#).unwrap();
+        writeln!(tmpfile, r#"{{"key1": 123}}"#).unwrap();
+        tmpfile.seek(SeekFrom::Start(0)).unwrap();
+        let reader = io::BufReader::new(tmpfile);
+
+        let expected: Vec<IdJSON> = vec![
+            (0.to_string(), json!({"key1": 123})),
+            (2.to_string(), json!({"key1": 123})),
+        ];
+
+        let args = Cli::default();
+        let errors = Rc::new(RefCell::new(vec![]));
+
+        let json_iter = parse_bufreader(&args, reader, &errors).unwrap();
+        assert_eq!(expected, json_iter.collect::<Vec<IdJSON>>());
+        assert!(errors.borrow().len() == 1)
+    }
+
+    #[test]
+    fn simple_expand_jsonpath_query() {
+        let json_iter_in: Vec<IdJSON> = vec![
+            (0.to_string(), json!({"key1": [1, 2, 3]})),
+            (1.to_string(), json!({"key2": 123})),
+            (2.to_string(), json!({"key1": [4, 5]})),
+        ];
+        let json_iter_in = json_iter_in.iter().cloned();
+
+        let mut args = Cli::default();
+        args.jsonpath = Some("$.key1[*]".to_string());
+        let settings = Settings::init(args).unwrap();
+        let errors = Rc::new(RefCell::new(vec![]));
+
+        let expected: Vec<IdJSON> = vec![
+            ("0:$.key1[*][0]".to_string(), json!(1)),
+            ("0:$.key1[*][1]".to_string(), json!(2)),
+            ("0:$.key1[*][2]".to_string(), json!(3)),
+            ("2:$.key1[*][0]".to_string(), json!(4)),
+            ("2:$.key1[*][1]".to_string(), json!(5)),
+        ];
+
+        let json_iter = expand_jsonpath_query(&settings, json_iter_in, &errors);
+        assert_eq!(expected, json_iter.collect::<Vec<IdJSON>>());
+        assert!(errors.borrow().len() == 1)
+    }
+
+    #[test]
+    fn simple_process_json_iterable() {
+        let json_iter_in: Vec<IdJSON> = vec![
+            (0.to_string(), json!({"key1": 123})),
+            (1.to_string(), json!({"key2": 123})),
+            (2.to_string(), json!({"key1": 123})),
+        ];
+        let json_iter_in = json_iter_in.iter().cloned();
+
+        let args = Cli::default();
+        let settings = Settings::init(args).unwrap();
+        let errors = Rc::new(RefCell::new(vec![]));
 
         let expected = FileStats {
             keys_count: IndexMap::from([("$.key1".to_string(), 2), ("$.key2".to_string(), 1)]),
@@ -578,9 +662,9 @@ mod tests {
             ..Default::default()
         };
 
-        let args = Cli::default();
-        let file_stats = parse_ndjson_file(&args, tmpfile).unwrap();
+        let file_stats = process_json_iterable(&settings, json_iter_in, &errors);
         assert_eq!(expected, file_stats);
+        assert!(errors.borrow().is_empty())
     }
 
     #[test]
@@ -628,23 +712,6 @@ mod tests {
 
         let args = Cli::default();
         let file_stats = parse_json_iterable_par(&args, iter).unwrap();
-        assert_eq!(expected, file_stats);
-    }
-
-    #[test]
-    fn bad_ndjson_file() {
-        let mut tmpfile: File = tempfile::tempfile().unwrap();
-        writeln!(tmpfile, "{{").unwrap();
-        tmpfile.seek(SeekFrom::Start(0)).unwrap();
-
-        let expected = FileStats {
-            bad_lines: vec![1],
-            line_count: 1,
-            ..Default::default()
-        };
-
-        let args = Cli::default();
-        let file_stats = parse_ndjson_file(&args, tmpfile).unwrap();
         assert_eq!(expected, file_stats);
     }
 
